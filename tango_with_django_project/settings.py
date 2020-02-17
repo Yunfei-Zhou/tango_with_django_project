95f0c9b81d108407565567b3298add81977a751f
Yunfei Zhou <2425106z@student.gla.ac.uk>
2020-02-17 06:35:19 +0000

    URL config

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3(+hkjtw@gbj7h1-hmj8c9)swh9!*4jc^!(yarp+fszr1g3tue'
+SECRET_KEY = 'pn!1wk$-@yt57ktv%r1$_ri(1z%xh_#if22*mq*y130^@38jgk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rango',
 ]
 
 MIDDLEWARE = [
